758f661be4f6505ccd71d166b17fa1cea31e1a8f
heanjio <henryj@unninorte.edu.co>
2022-11-23 20:11:27 -0500

    Inicio del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Home from './Components/Home'
+import Login from './Components/Login'
+import Navbar from './Components/Navbar'
+
+
+
+function App() {
+
+  return (
+    <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+    </Router>
+
+
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,23 @@
+import { NavLink } from 'react-router-dom'
+import './Navbar.css'
+const Navbar =() => {
+    return (
+        <nav className="navbar navbar-expand-lg bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">My Libray</a>
+                <div id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/">Home</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/login">Login</NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
